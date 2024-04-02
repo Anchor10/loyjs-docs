a202cde797a84b15482c36d7c2fa2597fcbc83c7
Anchor10 <1129713332@qq.com>
2024-04-02 09:18:35 +0800

    生日表表格编辑

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -41,12 +41,23 @@ export default defineConfig({
 			{
 				text: '备忘录',
 				items: [
-					{ text: '工作账号', link: '/markdown-examples' },
-					{ text: '生活账号', link: '/api-examples' },
+					{ text: '生日表', link: '/备忘录/生日表.md' },
+				],
+			},
+			{
+				text: '前端知识库',
+				items: [
+					{ text: 'javascript', link: '/前端知识库/javascript/JavaScript示例.md' },
+					{ text: 'vue', link: '/前端知识库/vue/vue示例.md' },
+					{ text: 'css', link: '/前端知识库/css/css示例.md' },
 				],
 			},
 		],
 
 		socialLinks: [{ title: '起始页', link: 'https://home.loyjs.com' }, { icon: 'github', link: 'https://gitee.com/anchor10' }],
+		footer: {
+			message: '去做你害怕的事，去见你害怕的人，这就是成长。 ----末那大叔',
+			copyright: 'Copyright © 2023-present By 觅兔先生'
+		}
 	},
 });
